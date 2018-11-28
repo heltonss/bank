51d4534e3642c12cc731856aed345ebe83ce6ab3
Heltonss <Helton@heltonsouza.com.br>
2018-11-28 08:12:10 -0200

    first commit

---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public abstract class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+
+        Funcionario(double salario, string cpf){
+            CPF = cpf;
+            Salario = salario;
+            TotalFuncionarios++;
+        }
+
+        public abstract void AumentarSalario();
+
+        public abstract double GetBonificacao();
+
+
+        public static int TotalFuncionarios { get; private set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+
+        }
+    }
+}
